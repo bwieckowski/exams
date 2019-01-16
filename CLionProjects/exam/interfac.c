b9834f460a2bbaff79cbf8605d22f745380c5540
Bartek <bartek.wieckowski@gmail.com>
2019-01-16 14:46:53 +0100

    dodano readme

---
+++ CLionProjects/exam/interfac.c
@@ -0,0 +1,5 @@
+//
+// Created by Bartek on 12.05.2018.
+//
+
+#include "interfac.h"
---
+++ CLionProjects/exam/interfac.h
@@ -0,0 +1,8 @@
+//
+// Created by Bartek on 12.05.2018.
+//
+
+#ifndef EXAM_INTERFAC_H
+#define EXAM_INTERFAC_H
+
+#endif //EXAM_INTERFAC_H
